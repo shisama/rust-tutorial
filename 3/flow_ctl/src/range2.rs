2c57eb456cbce86bc0aaf1c85d51b1d79ba8bc62
shisama <cherrydog07@gmail.com>
2019-01-18 00:12:29 +0900

    Add variables and flow control

---
+++ 3/flow_ctl/src/range2.rs
@@ -0,0 +1,6 @@
+fn main() {
+    for number in (1..=3).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF");
+}
---
+++ 3/variables/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+}
