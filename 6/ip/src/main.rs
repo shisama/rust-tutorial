297c8b755acbfb9d97a6dacef7e48fbfab5023cc
shisama <cherrydog07@gmail.com>
2019-02-22 12:06:22 +0900

    Update Enum with String data

--- 6/ip/src/main.rs
+++ 6/ip/src/main.rs
@@ -1,6 +1,6 @@
 enum IpAddrKind {
-    V4,
-    V6,
+    V4(String),
+    V6(String),
 }
 
 struct IpAddr {
@@ -8,12 +8,6 @@ struct IpAddr {
     address: String,
 }
 
-let home = IpAddr {
-    kind: IpAddrKind::V4,
-    address: String::from("127.0.0.1"),
-}
+let home = IpAddr::V4(String::from("127.0.0.1"));
 
-let loopback = IpAddr {
-    kind: IpAddrKind::V6,
-    address: String::from("::1"),
-}
+let loopback = IpAddr::V6(String::from("::1"));
