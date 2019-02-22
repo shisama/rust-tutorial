d3f9b05dab6b12195013fd653a76d173fe6d4e88
shisama <cherrydog07@gmail.com>
2019-02-22 12:09:50 +0900

    Fix IpAddr enum

--- 6/ip/src/main.rs
+++ 6/ip/src/main.rs
@@ -1,13 +1,10 @@
-enum IpAddrKind {
+enum IpAddr {
     V4(String),
     V6(String),
 }
 
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+fn main() {
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+
+    let loopback = IpAddr::V6(String::from("::1"));
 }
-
-let home = IpAddr::V4(String::from("127.0.0.1"));
-
-let loopback = IpAddr::V6(String::from("::1"));
