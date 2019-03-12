156ee1f825bb0d8699d7beac18ae437cbb54e4d1
shisama <cherrydog07@gmail.com>
2019-03-12 12:14:59 +0900

    Add match_option

---
+++ 6/match_option/src/main.rs
@@ -0,0 +1,26 @@
+fn plus_one(x: Option<u32>) -> Option<u32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn is_odd(some_u8_value: u8) {
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (), // 何もしないときの`_`プレースホルダ
+    }
+}
+
+fn main() {
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("{:?}", six);
+    println!("{:?}", none);
+
+    is_odd(7);
+}
