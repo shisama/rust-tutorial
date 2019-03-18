3bbb24d6dddd5ee2570fcdee2cc368f7d176e686
shisama <cherrydog07@gmail.com>
2019-03-18 15:08:32 +0900

    Add communicator

---
+++ 7/communicator/src/lib.rs
@@ -0,0 +1,22 @@
+pub mod client;
+
+mod network;
+
+mod outermost {
+    pub fn middle_function() {}
+
+    fn middle_secret_function() {}
+
+    mod inside {
+        pub fn inner_function() {}
+
+        fn secret_function() {}
+    }
+}
+
+fn try_me() {
+    outermost::middle_function();
+    outermost::middle_secret_function();
+    //outermost::inside::inner_function(); // エラーになる
+    //outermost::inside::secret_function(); // エラーになる
+}
