bcadacaf1b23f909d0e853350f7bd8ecd3f1574b
Masashi Hirano <cherrydog07@gmail.com>
2019-04-11 00:35:43 +0900

    Update vector with macro, iterate, enum

--- 8/vector/src/main.rs
+++ 8/vector/src/main.rs
@@ -4,4 +4,47 @@ fn main() {
     vec.push(2);
     vec.push(3);
     println!("{:?}", vec);
+
+    useMacro();
+    iterate();
+    enumerate();
+}
+
+fn useMacro() {
+    let v = vec![1, 2, 3, 4, 5];
+    let third: &i32 = &v[2];
+    println!("third: {:?}", third);
+
+    let third: Option<&i32> = v.get(2);
+    println!("optional third: {:?}", third);
+}
+
+fn iterate() {
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{}", i);
+    }
+
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+    }
+}
+
+#[derive(Debug)]
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
+
+fn enumerate() {
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+    for i in &row {
+        println!("{:?}", i);
+    }
 }
